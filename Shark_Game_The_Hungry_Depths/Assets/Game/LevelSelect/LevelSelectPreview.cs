e4e838ee13032e14bfc23d1856a92b30a6e7ebc5
GlebLava <60477842+GlebLava@users.noreply.github.com>
2024-03-21 10:41:46 +0100

    Preview Level Widget done

---
+++ Shark_Game_The_Hungry_Depths/Assets/Game/LevelSelect/LevelSelectPreview.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+public class LevelSelectPreview : MonoBehaviour, IPointerClickHandler
+{
+    public TMP_Text title;
+    public Image levelImage;
+
+    public UnityEvent OnClick;
+
+    private void Awake()
+    {
+        SignalBus.OnLevelSelected += OnLevelSelected;
+        OnLevelSelected(GameManager.instance.gameData.selectedLevel);
+    }
+
+    void OnLevelSelected(string levelName)
+    {
+        var level = GameManager.instance.levelSelectItemScriptableObjects.Find((so) => so.name == levelName);
+        if (level == null)
+        {
+            title.text = "Error: please reselect a level";
+        }
+
+        title.text = level.levelTitle;
+        levelImage.sprite = level.levelSprite;
+    }
+
+    private void OnDestroy()
+    {
+        SignalBus.OnLevelSelected -= OnLevelSelected;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        OnClick?.Invoke();
+    }
+}
